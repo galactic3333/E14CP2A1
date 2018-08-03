dc10975818b1fab4589ed8b83f26dff8be9e37b4
Gabriel Alarcon <galarconesp@gmail.com>
2018-08-03 06:42:30 -0400

    commit 02: Destroy employee completed

---
+++ app/controllers/employees_controller.rb
@@ -0,0 +1,20 @@
+class EmployeesController < ApplicationController
+
+  def create
+    @company = Company.find(params[:company_id])
+    @company.employees.build(employee_params)
+    @company.save
+    redirect_to company_path(@company)
+  end
+
+  def destroy
+    @employee = Employee.find(params[:id])
+    @employee.destroy 
+    redirect_to companies_path
+  end
+
+  private
+    def employee_params
+      params.require(:employee).permit(:first_name, :last_name, :email, :area_id)
+    end
+end
